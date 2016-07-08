902cc028a02c7adef0ab1c710b9f05fdf3d42dc0
JL <jeanlouis02300@gmail.com>
2016-07-08 18:13:53 +0200

    changement de style
    debut style on page

--- js/script.js
+++ js/script.js
@@ -13,13 +13,36 @@ $( document ).ready(function() {
         showCursor: true,
       });
 
-	// Changing the defaults
-	window.sr = ScrollReveal();
+    $("#demosMenu").change(function(){
+      window.location.href = $(this).find("option:selected").attr("id") + '.html';
+    });
 
-	sr.reveal('.RevealDiv', { duration: 700,  });
-	sr.reveal('.RevealListe', { delay: 300, duration: 700,  rotate: {x: 90}});
-	sr.reveal('.RevealImages', {origin : 'right', delay: 300, duration: 700,  rotate: {x: 90}});
-	sr.reveal('.RevealLeft', {origin :'left', delay: 300, duration: 900,  });
-	sr.reveal('.RevealRight', {origin : 'right', delay: 300, duration: 900,  });
+       $('#fullpage').fullpage({
+                'verticalCentered': false,
+                'css3': true,
+                'navigation': true,
+                'navigationPosition': 'right',
+
+                'afterLoad': function(anchorLink, index){
+                    if(index == 2){
+                        $('#presentation, #iphone2, #iphone4').addClass('active');
+                    }
+                },
+
+                'onLeave': function(index, nextIndex, direction){
+                    if (index == 3 && direction == 'down'){
+                        $('.section').eq(index -1).removeClass('moveDown').addClass('moveUp');
+                    }
+                    else if(index == 3 && direction == 'up'){
+                        $('.section').eq(index -1).removeClass('moveUp').addClass('moveDown');
+                    }
+
+                    $('#staticImg').toggleClass('active', (index == 2 && direction == 'down' ) || (index == 4 && direction == 'up'));
+                    $('#staticImg').toggleClass('moveDown', nextIndex == 4);
+                    $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
+                }
+            });
+
+
 
   });
